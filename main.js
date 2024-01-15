f4bd8d71d8d2b55cea8c6458366c416f2289ed1f
Funksicle <waderr@duck.com>
2024-01-15 20:35:34 +0800

    add dictionary for real

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { config } from "./lib/config.js";
-import { JackieTime } from "./lib/time.js";
-import { JackieCurrency } from "./lib/currency.js";
+import { jackieTime } from "./lib/time.js";
+import { jackieCurrency } from "./lib/currency.js";
+import { jackieDictionary } from "./lib/dictionary.js";
 
 const app = express();
-const jackieTime = new JackieTime();
-const jackieCurrency = new JackieCurrency();
 
-app.get(jackieTime.route, jackieTime.callback);
-app.get(jackieCurrency.route, jackieCurrency.callback);
+jackieDictionary.register(app);
+jackieTime.register(app);
+jackieCurrency.register(app);
 
 app.listen(config.get("port"), function () {
     console.log(`Listening on port ${config.get("port")}`);
