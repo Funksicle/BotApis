c6c46f3f14faa77c3015f7fbaad15df72b7be8ec
Funksicle <waderr@duck.com>
2024-01-15 10:58:30 +0800

    add currency converter

--- main.js
+++ main.js
@@ -1,41 +1,15 @@
 import express from "express";
 import { config } from "./lib/config.js";
+import { JackieTime } from "./lib/time.js";
+import { JackieCurrency } from "./lib/currency.js";
 
 const app = express();
+const jackieTime = new JackieTime();
+const jackieCurrency = new JackieCurrency();
 
-app.get("/jackietime", async function (req, res) {
-    try {
-        if (req.query["tz"] === undefined || req.query["field"] === undefined)
-            return console.error("No fields specified.");
-        const location = req.query["tz"];
-        const timeResponse = await webGet(`https://timeapi.io/api/Time/current/zone?timeZone=${location}`);
-        const json = JSON.parse(timeResponse);
-        const field = req.query["field"];
-        if (field === "dateTime") {
-            res.end(json[field].split("T").join(" ").split(".")[0]);
-            return;
-        }
-        if (json[field] !== undefined) {
-            res.end(json[field]);
-            return;
-        }
-        const formattedDate = `${json.date} ${json.time}`
-        res.end(formattedDate);
-    } catch (err) {
-        console.log(err);
-    }
-});
+app.get(jackieTime.route, jackieTime.callback);
+app.get(jackieCurrency.route, jackieCurrency.callback);
 
-const server = app.listen(config.get("port"), function () {
+app.listen(config.get("port"), function () {
     console.log(`Listening on port ${config.get("port")}`);
 });
-
-async function webGet(url) {
-    try {
-        const resp = await fetch(url);
-        const text = await resp.text();
-        return text;
-    } catch (err) {
-        console.log(err);
-    }
-}
