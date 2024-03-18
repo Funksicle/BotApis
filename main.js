22a59cab441050da5fd47c9109acd7181cb9d345
Funksicle <waderr@duck.com>
2024-03-18 14:17:43 +0800

    add the ability to set currency by variable

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import express from "express";
 import { config } from "./lib/config.js";
 import { jackieTime, jackieVarTime } from "./lib/time.js";
 import { jackieWeather } from "./lib/weather.js";
-import { jackieCurrency } from "./lib/currency.js";
+import { jackieCurrency, jackieVarCurrency } from "./lib/currency.js";
 import { jackieDictionary } from "./lib/dictionary.js";
 import { jackieUrbanDictionary } from "./lib/urbandictionary.js";
 import { jackieSetValue } from "./lib/set.js";
@@ -15,6 +15,7 @@ jackieTime.register(app);
 jackieVarTime.register(app);
 jackieWeather.register(app);
 jackieCurrency.register(app);
+jackieVarCurrency.register(app);
 jackieSetValue.register(app);
 
 app.listen(config.get("port"), function () {
