bf96444f3b6eecbf15abf3719f60517c318181df
Funksicle <waderr@duck.com>
2024-03-18 09:43:11 +0800

    add testing thing.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import { jackieWeather } from "./lib/weather.js";
 import { jackieCurrency } from "./lib/currency.js";
 import { jackieDictionary } from "./lib/dictionary.js";
 import { jackieUrbanDictionary } from "./lib/urbandictionary.js";
+import { jackieTesting } from "./lib/testing.js";
 
 const app = express();
 
@@ -13,6 +14,7 @@ jackieUrbanDictionary.register(app);
 jackieTime.register(app);
 jackieWeather.register(app);
 jackieCurrency.register(app);
+jackieTesting.register(app);
 
 app.listen(config.get("port"), function () {
     console.log(`Listening on port ${config.get("port")}`);
