3ab258a244001e36f335a384858ff14f4fd2a643
Funksicle <waderr@duck.com>
2024-03-29 23:58:02 +0800

    replace express with a basic server and fix misc stuff

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-import express from "express";
-import { config, logger, commandCache } from "./lib/deps.js";
+import { commandCache, turtleServer } from "./lib/deps.js";
 import "./lib/time.js";
 import "./lib/set.js";
 import "./lib/dictionary.js";
@@ -7,9 +6,4 @@ import "./lib/urbandictionary.js";
 import "./lib/google.js";
 // import "./lib/aichat.js";
 
-const app = express();
-const port = config.get("port") || 8980;
-
-commandCache.forEach(cmd => cmd.register(app));
-
-app.listen(port, () => logger.info(`Listening on ${config.get("host")}:${port}`));
+commandCache.forEach(turtleServer.register);
