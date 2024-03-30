fc4bc952b818bc45dd4cf8a7a35cf756e8119d03
Funksicle <waderr@duck.com>
2024-03-30 16:53:50 +0800

    misc, eslint

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-import { commandCache, turtleServer } from "./lib/deps.js";
+import { commandCache, config, TurtleServer } from "./lib/deps.js";
 import "./lib/time.js";
 import "./lib/set.js";
 import "./lib/dictionary.js";
 import "./lib/urbandictionary.js";
 import "./lib/google.js";
 // import "./lib/aichat.js";
+
+const turtleServer = new TurtleServer(config.get("port"));
 
 commandCache.forEach(turtleServer.register);
